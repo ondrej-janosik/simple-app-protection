c01dd9d0845cb365da40eeb1a62c2042e089a828
Ondřej Janošík <ondrej.janosik@notum.cz>
2019-03-22 14:28:05 +0100

    nev version

--- index.d.ts
+++ index.d.ts
@@ -5,5 +5,10 @@ interface Options {
     promptText: string;
     turnOffInDevelopment: boolean;
 }
-export declare const protect: (password: string, options?: Options) => void;
+/**
+ * Protect app by prompting for password
+ * @param password
+ * @param options - options. Default values: { saveToCookie: true, cookieName: 'simple-app-protection', cookieExpirationInDays: 7, promptText: 'Enter password', turnOffInDevelopment: true}
+ */
+export declare const protect: (password: string, options?: Partial<Options>) => void;
 export {};
--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var js_cookie_1 = require("js-cookie");
 var defaultOptions = {
@@ -14,17 +25,23 @@ var promptPass = function (password, text) {
         psw = prompt(text);
     }
 };
+/**
+ * Protect app by prompting for password
+ * @param password
+ * @param options - options. Default values: { saveToCookie: true, cookieName: 'simple-app-protection', cookieExpirationInDays: 7, promptText: 'Enter password', turnOffInDevelopment: true}
+ */
 exports.protect = function (password, options) {
     if (options === void 0) { options = defaultOptions; }
+    var currentOptions = __assign({}, defaultOptions, options);
     if (options.turnOffInDevelopment && process.env.NODE_ENV === 'development') {
         return;
     }
-    if (!js_cookie_1.get(options.cookieName)) {
-        promptPass(password, options.promptText);
+    if (!js_cookie_1.get(currentOptions.cookieName)) {
+        promptPass(password, currentOptions.promptText);
     }
-    if (options.saveToCookie) {
-        js_cookie_1.set(options.cookieName, 'authorized', {
-            expires: options.cookieExpirationInDays,
+    if (currentOptions.saveToCookie) {
+        js_cookie_1.set(currentOptions.cookieName, 'authorized', {
+            expires: currentOptions.cookieExpirationInDays,
         });
     }
 };
