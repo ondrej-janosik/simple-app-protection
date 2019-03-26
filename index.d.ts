a42502589e67bfe83344dc2d6168617047d02847
Tomas Jakubek <tmsjkbk@gmail.com>
2019-03-26 19:35:21 +0100

    possiblity to add multiple passwords

--- index.d.ts
+++ index.d.ts
@@ -7,8 +7,9 @@ interface Options {
 }
 /**
  * Protect app by prompting for password
- * @param password
+ * @param password - one or more valid passwords
  * @param options - options. Default values: { saveToCookie: true, cookieName: 'simple-app-protection', cookieExpirationInDays: 7, promptText: 'Enter password', turnOffInDevelopment: true}
+ * @param callback - function. It will be fired after successful validation (after typing valid password or after cookies retrieval). Typed password will be passed to callback
  */
-export declare const protect: (password: string, options?: Partial<Options>) => void;
+export declare const protect: (password: string | string[], options?: Partial<Options>, callback?: ((password: string) => void) | undefined) => void;
 export {};
